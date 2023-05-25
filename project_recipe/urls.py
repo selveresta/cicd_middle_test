d8beb78efd5bdc32fa55a93a4e3292e5a9872e0b
selveresta <artem.lypnyk@softsculptor.net>
2023-05-25 14:48:21 +0300

    Commit

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-
+from recipe.views import main, recipeDetail, categoryList, categoryDetail
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', main, name="main"),
+    path('recipeDetail/', recipeDetail, name="recipeDetail"),
+    path('categoryList/', categoryList, name="categoryList"),
+    path('categoryDetail/', categoryDetail, name="categoryDetail"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,25 @@
+import random
 
+from django.shortcuts import render
+from .models import Recipe
+from .models import Category
+
+
+# Create your views here.
+def main(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'main.html', context={'recipes': recipes})
+
+
+def recipeDetail(request):
+    recipe = Recipe.objects.get(title="IngredientsIngredientsIngredients")
+    return render(request, 'recipe_detail.html', context={'recipe': recipe})
+
+
+def categoryList(request):
+    categoryList = Category.objects.all()
+    return render(request, 'category_list.html', context={'categories': categoryList})
+
+def categoryDetail(request):
+    category = Category.objects.get(name="Category1")
+    return render(request, 'category_detail.html', context={'category': category})
